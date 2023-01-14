572331dd7f6cfa81f11ab94e5b8b262e52b98a19
Gyuho Lee <gyuho.lee@avalabs.org>
2023-01-14 13:51:17 +0800

    unit: sync
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- scripts/tests.unit.sh
+++ scripts/tests.unit.sh
@@ -12,14 +12,17 @@ RUST_LOG=debug cargo test --workspace \
 --features avalanche-types/cert \
 --features avalanche-types/codec_base64 \
 --features avalanche-types/codec_big_int \
+--features avalanche-types/evm \
 --features avalanche-types/jsonrpc_client \
 --features avalanche-types/kms_aws \
 --features avalanche-types/libsecp256k1 \
 --features avalanche-types/message \
 --features avalanche-types/mnemonic \
+--features avalanche-types/subnet \
 --features avalanche-types/subnet_evm \
 --features avalanche-types/wallet \
 --features avalanche-types/wallet_evm \
+--features avalanche-types/xsvm \
 -- --show-output
 
 echo "ALL SUCCESS!"
